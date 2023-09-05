16db5a37ac6c4bb0dc21176a612a72daad0a4681
Dico Diaz <dicodiaz@gmail.com>
2023-09-05 09:42:53 -0500

    Add error handling for API fetching

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,28 +1,53 @@
 import { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Container, Row, Spinner } from 'react-bootstrap';
 import './App.css';
 import Campaign from './components/Campaign';
 
 const App = () => {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchAndSetData = async () => {
-      const localStorageData = localStorage.getItem('data');
-      if (localStorageData) {
-        setData(JSON.parse(localStorageData));
-        return;
-      }
+  const fetchAndSetData = async () => {
+    setError(null);
 
+    const localStorageData = localStorage.getItem('data');
+    if (localStorageData) {
+      setData(JSON.parse(localStorageData));
+      return;
+    }
+
+    try {
       const response = await fetch('https://www.plugco.in/public/take_home_sample_feed');
       const responseData = await response.json();
       setData(responseData);
       localStorage.setItem('data', JSON.stringify(responseData));
-    };
+    } catch {
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
     fetchAndSetData();
   }, []);
 
-  if (!data) return null;
+  if (error) {
+    return (
+      <Container className="vh-100 d-flex justify-content-center align-items-center">
+        <Row className="mx-0 g-0">
+          <h1>Apologies, there has been an error</h1>
+          <Button onClick={fetchAndSetData}>Try again</Button>
+        </Row>
+      </Container>
+    );
+  }
+
+  if (!data) {
+    return (
+      <Container className="vh-100 d-flex justify-content-center align-items-center">
+        <Spinner />
+      </Container>
+    );
+  }
 
   return (
     <Container className="py-5 px-0">
